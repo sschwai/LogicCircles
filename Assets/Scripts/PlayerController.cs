7e3b1bb51b33f315eee7e4580f22ce9734322c38
sschwai <sydney.schwai@gmail.com>
2023-04-07 16:20:01 -0400

    Added Player sprite and PlayerController script with movement and rotation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //declaring variables
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed); //movement
+        transform.Rotate(Vector3.forward); //rotates back is right, forward is left
+    }
+}
