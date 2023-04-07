550707b53d00edd3c0efc924c421dd81272e7cf0
sschwai <sydney.schwai@gmail.com>
2023-04-07 17:12:00 -0400

    Added Player, PlayerController, Trigger Collisions to Player and objects, completed puzzle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     //declaring variables
     public float speed;
+    public bool right;
+    public string GameScene;
 
     void Start()
     {
-        
+        if (!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
     }
 
     void Update()
     {
         transform.Translate(Vector3.right * speed); //movement
-        transform.Rotate(Vector3.forward); //rotates back is right, forward is left
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        { Restart(); }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Collided with " + other.gameObject.name);
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        //Debug.Log("Changing player direction: " + dn.current);
+
+        //check direction, rotation back is right, forward is left
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+        if (dn.current == "right")
+        {
+            dir = Vector3.back;
+            rot = 90;
+        }
+        if (dn.current == "left")
+        {
+            dir = Vector3.forward;
+            rot = 90;
+        }
+
+        transform.Rotate(dir * rot);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(GameScene);
     }
 }
